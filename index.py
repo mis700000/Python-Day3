68cb49993bf1ac9c5719b77b621a1b7621422390
mohmad sheikh <mohmad sheikh>
2017-03-08 19:50:02 +0530

    Added day 4 repo

---
+++ index.py
@@ -0,0 +1,13 @@
+#The method index() returns the lowest index in list that obj appears.
+
+#Following is the syntax for index() method list.index(obj)
+
+
+l=["sai","krishna",123,567]
+print l.index("krishna")
+print l.index(123)
+
+
+
+
+
